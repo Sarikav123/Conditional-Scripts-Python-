396253cb518bd01c5b9dfab11128dff0f95ebed1
Sarikav123 <72382945+Sarikav123@users.noreply.github.com>
2025-10-01 17:42:36 +0530

    Create Pattern.py

---
+++ Pattern.py
@@ -0,0 +1,10 @@
+
+#
+# Exercise 10 Write a program to print the following pattern:
+#
+# 5 4 3 2 1 4 3 2 1 3 2 1 2 1 1
+
+for n in range(5,0,-1):
+    for i in range(n,0,-1):
+        print(i, end=' ')
+    print()
