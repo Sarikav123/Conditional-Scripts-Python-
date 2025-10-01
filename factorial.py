2aba3a5ab2d8f5431539f3c253896853de93df97
Sarikav123 <72382945+Sarikav123@users.noreply.github.com>
2025-10-01 17:45:55 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,15 @@
+
+
+inputbyuser = int(input("Enter a number to find the factorial: "))
+
+a = inputbyuser
+
+fv = 1
+
+for i in range(1,a +1):
+    fv = fv * i
+
+
+print("The factorial of", inputbyuser, "is", fv)
+
+
